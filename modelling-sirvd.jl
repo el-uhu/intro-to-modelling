6792c11c2c5809ce57fc27de51ef8e4ec57f42f3
Lukas Hutter <luki@biotop.co>
2021-04-03 14:25:20 +0200

    julia v1.6 update

--- modelling-sirvd.jl
+++ modelling-sirvd.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.12.21
+# v0.14.0
 
 using Markdown
 using InteractiveUtils
@@ -167,12 +167,12 @@ sol2(0:tmax2, Val{1}, idxs=2)
 # ╔═╡ Cell order:
 # ╠═430d5134-92af-11eb-1fcc-d71f5216d62a
 # ╟─5adc7e34-92af-11eb-2cfe-67899a4a0291
-# ╠═7a19b238-92af-11eb-30fd-b189d06a0882
+# ╟─7a19b238-92af-11eb-30fd-b189d06a0882
 # ╟─7ec0712a-92af-11eb-2144-15d33af27c3d
 # ╟─88b411f0-92af-11eb-2abf-151b2bfb313b
-# ╟─230527fa-92b3-11eb-308f-ffab3777029c
+# ╠═230527fa-92b3-11eb-308f-ffab3777029c
 # ╟─8f867f9a-92af-11eb-3671-43904aa74d8b
-# ╠═96d8a5c2-92af-11eb-1d3f-9bdd035eafec
+# ╟─96d8a5c2-92af-11eb-1d3f-9bdd035eafec
 # ╟─9c0dfe82-92af-11eb-3192-49c2deac6e9b
 # ╟─a1e4ca5e-92af-11eb-1c40-410a8d3abaca
 # ╟─ada6253e-92af-11eb-0e32-d9760ab88271
